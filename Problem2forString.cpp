a0b324aba552f65d722381f45d8b994ce326493b
Kanishk raj singh jhala <140880609+JhalaKanishk@users.noreply.github.com>
2023-12-17 21:52:09 +0530

    Update Problem2forString.cpp

--- Problem2forString.cpp
+++ Problem2forString.cpp
@@ -1,17 +1,20 @@
 #include<iostream>
 using namespace std;
 //to reverse a string
-class String
+class Solution
 {
     public:
     string reverseWord(string str)
     {
-        // Your code goes here
+        // if the size of string is too small then we can't reverse it
         if(str.size() == 0 || str.size() == 1)
             return str;
+
+        //we are using recursion here Recursion : function calling itself is known as recursion
         return reverseWord(str.substr(1))+str[0];
     }
 };
+
 
 
 int main(){
@@ -21,8 +24,8 @@ int main(){
     string demostring;
     cin >> demostring;//taking input
     cout << "String : " << demostring << endl;
-    //creating object of a string class and calling the function
-    String object;
+    //creating object of a Solution class and calling the function
+    Solution object;
     cout << "Reverse String : " << object.reverseWord(demostring);//passing the input string to the function
 
     return 0;
