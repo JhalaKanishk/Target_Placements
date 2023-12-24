f394507210ce0d73f9f53158a2e29b61deea06fd
Kanishk raj singh jhala <140880609+JhalaKanishk@users.noreply.github.com>
2023-12-24 07:56:32 +0530

    Create MajorQue.cpp

---
+++ MajorQue.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include <bits/stdc++.h>//this package is to use vectors
+#include<math.h>
+using namespace std;
+
+class Solution{
+    public :
+        int findSum(int arr[], int n){
+            //complete the function
+        }
+
+        string reverseWord(string str){
+            //complete the function
+        }
+
+        int maxSubArray(vector<int>& nums){
+            //complete the function
+        }
+
+        bool containsDuplicate(vector<int>& nums){
+            //complete the function
+        }
+
+        int search(vector<int>& nums, int target){
+            //complete the function
+        }
+
+        int maxProfit(vector<int>& prices){
+            //complete the function
+        }
+
+        int kthSmallest(int arr[], int l, int r, int k) {
+            //complete the function
+        }
+
+        int findKthLargest(vector<int>& nums, int k){
+            //complete the function
+        }
+};
+
+int main(){
+    cout << "Major Question Based on overall Week : " << endl;
+    Solution obj;
+    //complete the code
+    return 0;
+}
